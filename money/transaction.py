884b6c83e19086a5d7b3f19d3fae34084f363af5
Mark Allison <mallison77@gmail.com>
2012-02-04 22:04:14 +0000

    added back untagged transactions

--- money/transaction.py
+++ money/transaction.py
@@ -1,36 +1,43 @@
 """Helper functions for handling sets of transactions"""
 
+from operator import itemgetter
+
 from django.db.models import Sum
 
-from money.models import Payment, Tag
+from money.models import Payment, Tag, Transaction
 
 
 def totals_for_tags(transactions):
-    for name, total in (
-        Tag.objects
-        .filter(transaction__in=transactions)
-        .distinct()
-        .annotate(sum=Sum('transaction__amount'))
-        .order_by('sum')
-        .values_list('name', 'sum')
-    ):
-        # TODO can I do the math in the query?
-        yield name, total / 100.0
-        # TODO untagged transactions
+    totals = [(name, total / 100.0) for name, total in (
+            Tag.objects
+            .filter(transaction__in=transactions)
+            .distinct()
+            .annotate(sum=Sum('transaction__amount'))
+            .order_by('sum')
+            .values_list('name', 'sum')
+            )
+              ]
+    totals.append(('misc', sum(
+                transactions.filter(
+                    tags__isnull=True
+                    ).values_list('amount', flat=True)) / 100.0))
+    totals.sort(key=itemgetter(1))
+    return totals
 
 
 def in_and_out(transactions):
     for selector in ('amount__gte', 'amount__lt'):
         filtered_transactions = transactions.filter(
             **{selector: 0}
-              ).exclude(
-            # exclude savings as it's not income
-            tags__name="savings").values_list('amount', flat=True)
+            #   ).exclude(
+            # # exclude savings as it's not income
+            # tags__name="savings"
+            ).values_list('amount', flat=True)
         # TODO: no ORM way to do the summing?
         yield sum(filtered_transactions) / 100.0
     yield -sum(
         transactions.filter(tags__name="savings")
-        .values_list('amount', flat=True)) / 100
+        .values_list('amount', flat=True)) / 100.0
 
 
 def remaining_outgoings(transaction):
